2a45d7d6424fb552bc8b7b5948e5f967274dd09f
lucas <mcbride.lucas9@gmail.com>
2017-08-22 11:33:04 -0400

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn championship.wsgi --log-file -
---
+++ champ_app/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class ChampAppConfig(AppConfig):
+    name = 'champ_app'
