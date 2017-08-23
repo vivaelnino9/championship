206c1af27200ada477432674b9c47ad85190d11e
lucas <mcbride.lucas9@gmail.com>
2017-08-23 16:59:31 -0400

    remove requests

--- champ_app/views.py
+++ champ_app/views.py
@@ -1,17 +1,9 @@
 import json
-import requests
-from requests.auth import HTTPBasicAuth
 
 from django.shortcuts import render
 
 def index(request):
-    # title = '''{
-    #     "title":"TagPro Championship Series"
-    # }'''
-    # tour_data = json.loads(title)
-    # tournament = requests.put('https://bracketcloud.com/api/1.0/tournaments/103815?api_key=735b93738ff58c279fb465be2404dcc10dc6db8d',json=tour_data)
     return render(request,'index.html',{
-        # 'tournament':tournament.text,
     })
 
 def registration(request):
