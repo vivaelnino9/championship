fa52a478da87a54f23728740531565a054870f61
lucas <mcbride.lucas9@gmail.com>
2017-08-23 16:54:50 -0400

    add home page demo

--- champ_app/views.py
+++ champ_app/views.py
@@ -1,10 +1,18 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+import json
+import requests
+from requests.auth import HTTPBasicAuth
 
 from django.shortcuts import render
 
 def index(request):
-    return render(request,'index.html')
+    # title = '''{
+    #     "title":"TagPro Championship Series"
+    # }'''
+    # tour_data = json.loads(title)
+    # tournament = requests.put('https://bracketcloud.com/api/1.0/tournaments/103815?api_key=735b93738ff58c279fb465be2404dcc10dc6db8d',json=tour_data)
+    return render(request,'index.html',{
+        # 'tournament':tournament.text,
+    })
 
 def registration(request):
     return render(request,'registration.html')
