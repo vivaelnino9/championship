ef7b28a302d34236d49ba5aa3261cab0c74d6613
lucas <mcbride.lucas9@gmail.com>
2017-09-11 12:36:16 -0400

    add sidebar

---
+++ champ_app/static/js/sidebar.js
@@ -0,0 +1,14 @@
+function openNav() {
+    $('#mySidenav').animate({width:'280px'}, 100);
+    setTimeout(function(){
+      $('#mySidenav a, #mySidenav p').css('color','#b5b5b5');
+    }, 350);
+
+}
+
+function closeNav() {
+    $('#mySidenav a, #mySidenav p').css('color','#343a40');
+    setTimeout(function(){
+      $('#mySidenav').animate({width:'0px'}, 100);
+    }, 120);
+}
