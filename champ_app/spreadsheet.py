3d7a1807cee4483b3162995b365d35c22f74f993
lucas <mcbride.lucas9@gmail.com>
2017-09-11 12:36:53 -0400

    add spreadsheet api

---
+++ champ_app/spreadsheet.py
@@ -0,0 +1,17 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+# use creds to create a client to interact with the Google Drive API
+scope = ['https://spreadsheets.google.com/feeds']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+
+# Find a workbook by name and open the first sheet
+# Make sure you use the right name here.
+sheet = client.open("Team Form (Responses)").sheet1
+
+# Extract and print all of the values
+list_of_hashes = sheet.get_all_records()
+# print(list_of_hashes)
+cell = sheet.find("Rattpack")
+print("Found something at Row:%s Col:%s" % (cell.row, cell.col))
