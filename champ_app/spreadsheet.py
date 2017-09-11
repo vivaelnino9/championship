78b3969ae1ea8e2e380f71a6f453efe770f0b1b7
lucas <mcbride.lucas9@gmail.com>
2017-09-11 14:36:41 -0400

    get spreadsheet.py working

--- champ_app/spreadsheet.py
+++ champ_app/spreadsheet.py
@@ -5,16 +5,14 @@ import json
 from oauth2client.client import SignedJwtAssertionCredentials
 # use creds to create a client to interact with the Google Drive API
 scope = ['https://spreadsheets.google.com/feeds']
-private_key = os.environ['PRIVATE_KEY']
+client_email = os.environ['CLIENT_EMAIL']
 client_secret = json.loads(os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
-print(client_secret['private_key'])
-# creds = SignedJwtAssertionCredentials(client_email, private_key.encode(), scope)
-# gc = gspread.authorize(creds)
-# sheet = gc.open("Team Form (Responses)").sheet1
-# client = gspread.authorize(creds)
-#
-# sheet = client.open("Team Form (Responses)").sheet1
-#
-# list_of_hashes = sheet.get_all_records()
-# cell = sheet.find("Rattpack")
-# print("Found something at Row:%s Col:%s" % (cell.row, cell.col))
+private_key = client_secret['private_key']
+creds = SignedJwtAssertionCredentials(client_email, private_key, scope)
+gc = gspread.authorize(creds)
+sheet = gc.open("Team Form (Responses)").sheet1
+client = gspread.authorize(creds)
+
+list_of_hashes = sheet.get_all_records()
+cell = sheet.find("Rattpack")
+print("Found something at Row:%s Col:%s" % (cell.row, cell.col))
