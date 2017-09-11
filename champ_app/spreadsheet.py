38d86d3aa8edb40275e78319ba3fc3725aadeebc
lucas <mcbride.lucas9@gmail.com>
2017-09-11 14:33:23 -0400

    get spreadsheet.py working

--- champ_app/spreadsheet.py
+++ champ_app/spreadsheet.py
@@ -6,7 +6,8 @@ from oauth2client.client import SignedJwtAssertionCredentials
 # use creds to create a client to interact with the Google Drive API
 scope = ['https://spreadsheets.google.com/feeds']
 private_key = os.environ['PRIVATE_KEY']
-print(os.environ['GOOGLE_APPLICATION_CREDENTIALS']['private_key'])
+client_secret = json.loads(os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
+print(client_secret['private_key'])
 # creds = SignedJwtAssertionCredentials(client_email, private_key.encode(), scope)
 # gc = gspread.authorize(creds)
 # sheet = gc.open("Team Form (Responses)").sheet1
